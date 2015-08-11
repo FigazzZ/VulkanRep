cb5b7febfeb8f1c4e6dc30ba99f8ca62ecd49ab7
Juuso Kaitila <juuso.kaitila@bitwise.fi>
2015-08-11 17:11:37 +0300

    Initial commit that uses https://github.com/shu223/SlowMotionVideoRecorder as a base

---
+++ VVCamera/VVCamera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VVCamera
+//
+//  Created by Käyttäjä on 11.8.2015.
+//  Copyright (c) 2015 Bitwise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
